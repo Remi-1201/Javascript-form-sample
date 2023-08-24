dcb8e23d00f60a3b6c08e7c7589b66e19f918a3c
remi <remi.nkjm@gmail.com>
2023-08-24 21:16:35 +0900

    クリック時にフォームの内容を表示

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,7 +1,8 @@
 function confirmSubmit() {
   const forms = document.forms;
   forms[0].onsubmit = function(){
-    if (!(confirm("本当に送信しますか？"))) {
+    const name = forms[0].name.value;
+    if (!(confirm(`${name}さん、本当に送信しますか？`))) {
       alert("キャンセルされました");
       return false;
     }
